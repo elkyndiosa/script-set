6c7153a39a3037352958ad68b11b998eddcd21e0
Elkyn Gustavo Diosa Martinez <elkyn.diosa@linktic.co>
2021-09-08 10:55:59 -0500

    add header

--- upload-google-shets-2.js
+++ upload-google-shets-2.js
@@ -12,19 +12,29 @@ async function setData(){
         const body = data[key];
         console.log(JSON.stringify(body));
         const url = 'https://hook.integromat.com/9pn6exv6utwk5x490mzm53gqxxti79kg';
-        axios.post(url, JSON.stringify(body))
-        .then(function (response) {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            if (error.response) {
-                console.log(error.response.data);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log('Error', error.message);
-            }
+
+        response  = await axios({
+            method: 'post',
+            url: 'https://hook.integromat.com/9pn6exv6utwk5x490mzm53gqxxti79kg',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: body
         });
+        // console.log(response.json());
+        // axios.post(url, JSON.stringify(body))
+        // .then(function (response) {
+        //     console.log(response.data);
+        // })
+        // .catch(function (error) {
+        //     if (error.response) {
+        //         console.log(error.response.data);
+        //     } else if (error.request) {
+        //         console.log(error.request);
+        //     } else {
+        //         console.log('Error', error.message);
+        //     }
+        // });
 
 
         await sleep(4000);
